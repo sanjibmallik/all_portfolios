8ebf51d5bef5dcaa79a0b946d5c8132b9142dcf4
Sanjib Mallik <sanjib.mallik24@gmail.com>
2021-01-04 15:04:59 +0530

    V2 updated with components

--- portfolio_v2/src/app/app.module.ts
+++ portfolio_v2/src/app/app.module.ts
@@ -4,11 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AwardsComponent } from './components/awards/awards.component';
+import { InterestsComponent } from './components/interests/interests.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ContactMeComponent,
+    ProjectsComponent,
+    AwardsComponent,
+    InterestsComponent,
+    NavigationComponent
   ],
   imports: [
     HttpClientModule,
